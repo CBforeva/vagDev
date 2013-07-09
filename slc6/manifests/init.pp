6d213e5fe90e37f8fa71baaef2a402cdfcd08619
cbforeva <cbforeva@gmail.com>
2013-07-09 14:27:06 +0200

    Package provider + Oracle XE env setup.

--- slc6/manifests/init.pp
+++ slc6/manifests/init.pp
@@ -2,9 +2,15 @@ group { "puppet":
   ensure => "present",
 }
 
+Package { provider => "yum" }
+
 File { owner => 0, group => 0, mode => 0644 }
 
 file { '/etc/motd':
   content => "Welcome to your Vagrant-built virtual machine!
               Managed by Puppet.\n"
 }
+
+include oracle::server
+
+#include oracle::xe
